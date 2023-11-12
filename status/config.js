dd1450c17ebd0138d242838d913353186a6b148b
Fatmc123 <143685043+Fatmc123@users.noreply.github.com>
2023-11-12 12:52:07 +0800

    Add files via upload

---
+++ status/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '临云网站状态检测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm795517853-7cf86695bbf2ecf83318295e',
+    'm795563730-831498e8361b7744f9da2460',
+    'm795517867-7e7678729a5a0b0052b2a5a7',
+    'm795520098-437d96e951da6cbe10201b6e',
+    'm795191867-c027f3b527a401a2cde029ac',
+  ],
+
+  // 日志天数
+  CountDays: 45,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+  ],
+};
